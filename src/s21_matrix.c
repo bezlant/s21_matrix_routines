2e39af0a24c106b2af402da6cbd23550e21e82e9
Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>
2022-05-23 09:19:35 +0300

    determinant & makefile fix

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -85,6 +85,52 @@ int s21_transpose(matrix_t *A, matrix_t *result) {
     return OK;
 }
 
-/* int s21_calc_complements(matrix_t *A, matrix_t *result) { */
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+    if (A->rows != A->columns || result->rows != A->rows ||
+        result->columns != A->columns)
+        return CALC_ERROR;
+    for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+        }
+    }
+    return OK;
+}
 
-/* } */
+double s21_determinant(matrix_t *A) {
+    if (A->rows != A->columns)
+        return CALC_ERROR;
+    if (A->rows == 0)
+        return A->matrix[0][0];
+
+    double res = 0;
+    double **cofactor_mat = malloc(A->rows * A->columns * sizeof(double));
+    if (!cofactor_mat)
+        return MALLOC_FAILED;
+    int sign = 1;
+    for (int i = 0; i < A->rows; i++) {
+        cofactor(A->matrix, cofactor_mat, 0, i, A->rows);
+        A->rows--;
+        res += sign * A->matrix[0][i] * s21_determinant(A);
+        sign = -sign;
+    }
+    free(cofactor_mat);
+    return res;
+}
+
+void cofactor(double **mat, double **tmp, int skip_row, int skip_col,
+              int size) {
+    int i = 0;
+    int j = 0;
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (row != skip_row && col != skip_col) {
+                tmp[i][j] = mat[row][col];
+                j++;
+                if (j == size - 1) {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 4096
+
 enum { OK = 0, INCORRECT_MATRIX = 1, CALC_ERROR = 2, MALLOC_FAILED = 3 };
 enum { FAILURE = 0, SUCCESS = 1 };
 
@@ -23,9 +25,11 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_mult_number(matrix_t *A, double number, matrix_t *result);
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_transpose(matrix_t *A, matrix_t *result);
-int s21_calc_complements(matrix_t *A, matrix_t *result);
+double s21_determinant(matrix_t *A);
 // TODO
-int s21_determinant(matrix_t *A, double *result);
-int s21_inverse_matrix(matrix_t *A);
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+matrix_t s21_inverse_matrix(matrix_t *A);
 
+// helpers
+void cofactor(double **mat, double **tmp, int skip_row, int skip_col, int size);
 #endif  // SRC_S21_MATRIX_H
