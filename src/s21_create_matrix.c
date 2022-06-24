66277c419b9163841bd10fcddbc515e65f7583b2
bezlant <abezludniy@mail.ru>
2022-06-24 08:06:53 +0300

    malloc protection added

--- src/s21_create_matrix.c
+++ src/s21_create_matrix.c
@@ -3,15 +3,24 @@
 int s21_create_matrix(const int rows, const int columns, matrix_t *result) {
     if (rows <= 0 || columns <= 0)
         return INCORRECT_MATRIX;
+
     result->rows = rows;
     result->columns = columns;
     result->matrix = calloc(rows, sizeof(double *));
+
     if (!result->matrix)
         return MALLOC_FAILED;
+
     for (int i = 0; i < rows; i++) {
         result->matrix[i] = calloc(columns, sizeof(double));
-        if (!result->matrix[i])
+        if (!result->matrix[i]) {
+            /* clean-up before exit */
+            for (int j = 0; j < i; j++)
+                free(result->matrix[j]);
+            free(result->matrix);
+
             return MALLOC_FAILED;
+        }
     }
     return OK;
 }
